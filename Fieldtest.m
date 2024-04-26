c8941c20ad5ae222cb0cc06e6a474c9630ada745
Wouterklein03 <w.klein@student.tudelft.nl>
2024-04-26 13:47:27 +0200

    .

--- Fieldtest.m
+++ Fieldtest.m
@@ -1,3 +1,7 @@
 %  Testing of nesting states, parameters and inputs in matlab fields
 % Example GH.x.Temperature requests the state (x) Temperature out of the GH field
 
+GH.x = "states" ;
+GH.u = "inputs" ;
+GH.p = "parameters" ;
+GH.d = "defined inputs"
