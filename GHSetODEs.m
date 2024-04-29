0572374f45c7e65fae40941567d2b4b421048992
Wouterklein03 <w.klein@student.tudelft.nl>
2024-04-29 14:23:53 +0200

    .

--- GHSetODEs.m
+++ GHSetODEs.m
@@ -1,11 +1,11 @@
 %   dx/dt = function(GH, i)
 
-function AirTemperatureDot = AirTemperatureDot(GH, i)
-C_AirVolumeGH = GH.p.GHVolume * GH.p.rho_air * GH.p.cp_air ;
+function AirTemperatureDot = ODE_AirTemperature(GH, i)
+    C_AirVolumeGH = GH.p.GHVolume * GH.p.rho_air * GH.p.cp_air ;
 
-Q_RoofConv = GH.p.h_WallInside *(GH.x.WallTemperature(i) - GH.x.AirTemperature(i)) ;
+    Q_RoofConv = GH.p.h_WallInside *(GH.x.WallTemperature(i) - GH.x.AirTemperature(i)) ;
 
-Q = Q_RoofConv  ;% + Q_floor + Q_sky  + Q_lamp + Q_soil + Q_vent + Q_plant 
-AirTemperatureDot = Q/C_AirVolumeGH ;
+    Q = Q_RoofConv  ;% + Q_floor + Q_sky  + Q_lamp + Q_soil + Q_vent + Q_plant 
+    AirTemperatureDot = Q/C_AirVolumeGH ;
 
 end
