637db0a599e5c1ba728d659c07d320c17177ede0
ldenijs1234 <162971452+ldenijs1234@users.noreply.github.com>
2024-04-29 12:11:45 +0200

    u

--- ventilation_rate.m
+++ ventilation_rate.m
@@ -2,18 +2,19 @@
 A = 100; % m^2 (surface area of the greenhouse)
 roof_angle = 30; % degrees (angle of the roof)
 theta = 10; % degrees (angle of the window)
-number_of_windows = 2; 
+number_of_windows = 10; 
 l = 1.5; % m (length of the window)
 h = 0.5; % m (height of the window)
 A_0 = l * h; % m^2
-C_f = 0.6  % discharge of energy caused by friction in the window opening coefficient
+C_f = 0.6;  % discharge of energy caused by friction in the window opening coefficient
 g = 9.81; % m/s^2
 beta = 1/287; % K^-1 (thermal expansion coefficient of air)
 c_air = 1005; % J/(kg*K) (specific heat capacity of air)
 rho = 1.2; % kg/m^3 (density of air)
+t = 0:0.5:24; % time in hours
 
-%outputs
-function v_vent = calculate_ventilation_rate(A, roof_angle, theta, number_of_windows, l, h, C_f, g, beta, c_air, rho)
+%function
+function v_vent = calculate_ventilation_rate(A, roof_angle, theta, number_of_windows, l, h, C_f, g, beta, c_air, rho, t)
     % Constants
     Gl = 2.29 * 10^-2 * (1 - exp(-theta / 21.1)); % Leeside ventilation coefficient
     Gw = 1.2 * 10^-3 * theta * exp(theta / 211); % Windward side ventilation coefficient
@@ -24,10 +25,12 @@ function v_vent = calculate_ventilation_rate(A, roof_angle, theta, number_of_win
     % Area of roof
     A0 = l * h; % m^2 (Area of one window)
 
+    for i = 1:length(t)
     % Thermal difference (Tin - Tout), assumed to be 5 degrees Celsius for example
     Tin = 25; % Inside temperature in degrees Celsius
-    Tout = 20; % Outside temperature in degrees Celsius
+    Tout = 15 + 5*sin(2*pi*t(i)/(24*3600)); % Outside temperature in degrees Celsius
     delta_T = Tin - Tout; % Temperature difference in degrees Celsius
+    end
 
     % Ventilation rate due to wind
     v_wind = (Gl + Gw) * A0 * u; % m^3/s/window
@@ -48,6 +51,7 @@ function v_vent = calculate_ventilation_rate(A, roof_angle, theta, number_of_win
     disp(['vtemp: ', num2str(v_temp), ' m^3/s/window']);
 end
 
+%output
 v_vent = calculate_ventilation_rate(A, roof_angle, theta, number_of_windows, l, h, C_f, g, beta, c_air, rho);
 
 disp(['The ventilation rate is: ', num2str(v_vent), ' m^3/m^2/s']);
