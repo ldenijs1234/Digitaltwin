a0d0791ac9bf3e2f37da7f792450e6880b7c3c68
Wouterklein03 <w.klein@student.tudelft.nl>
2024-04-30 10:14:11 +0200

    add floor and

--- GHSetODEs.m
+++ GHSetODEs.m
@@ -7,7 +7,7 @@ function AirTemperatureDot = ODE_AirTemperature(GH, i)
     Q_Heating = GH.u.Heating(i) ;
     Q_RoofConv = GH.p.h_WallInside *(GH.x.WallTemperature(i) - GH.x.AirTemperature(i)) ;
 
-    Q = Q_RoofConv + Q_Heating ;% + Q_floor + Q_sky  + Q_lamp + Q_soil + Q_vent + Q_plant 
+    Q = Q_RoofConv + Q_Heating ;% + Q_floor + Q_sky  + Q_lamp + Q_soil + Q_vent + Q_plant + ...
     AirTemperatureDot = Q/C_AirVolumeGH ;
 
 end
@@ -20,11 +20,32 @@ function WallTemperatureDot = ODE_WallTemperature(GH, i)
     Q_solar = GH.p.AlfaGlass* GH.p.GHGroundArea*GH.d.SolarIntensity(i) ;  %!!!!!!
     Q_rad = - GH.p.EmittanceGlass * GH.p.StefBolzConst *GH.p.GHTotalArea*(GH.x.WallTemperature(i)^4 - GH.d.SkyTemperature(i)^4) ;
 
-    Q = Q_RoofConvIn + Q_RoofConvOut + Q_solar + Q_rad ;% + Q_floor + Q_sky  + Q_lamp + Q_soil + Q_vent + Q_plant 
+    Q = Q_RoofConvIn + Q_RoofConvOut + Q_solar + Q_rad ;% + Q_floor + Q_sky  + Q_lamp + Q_soil + Q_vent + Q_plant  + ....
     WallTemperatureDot = Q/C_WallsGH ;
 
 end
 
+
+function FloorTemperatureDot = ODE_FloorTemperature(GH, i)
+    C_FloorGH = 500 ;  %DUMMY!!!!!
+
+    Q_FloorConv = - GH.p.h_WallInside *(GH.x.FloorTemperature(i) - GH.x.AirTemperature(i)) ;
+
+    Q = Q_RoofConv + Q_Heating ;% + Q_floor + Q_sky  + Q_lamp + Q_soil + Q_vent + Q_plant + ....
+    FloorTemperatureDot = Q/C_FloorGH ;
+
+end
+
+function AirTemperatureDot = ODE_AirTemperature(GH, i)
+    C_AirVolumeGH = GH.p.GHVolume * GH.p.rho_air * GH.p.cp_air ;
+
+    Q_Heating = GH.u.Heating(i) ;
+    Q_RoofConv = GH.p.h_WallInside *(GH.x.WallTemperature(i) - GH.x.AirTemperature(i)) ;
+
+    Q = Q_RoofConv + Q_Heating ;% + Q_floor + Q_sky  + Q_lamp + Q_soil + Q_vent + Q_plant 
+    AirTemperatureDot = Q/C_AirVolumeGH ;
+
+end
 
 % Euler Integration
 for i = 1: (length(GH.d.Time)-1)
