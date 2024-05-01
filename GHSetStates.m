fed11bc27bfc891ed27e2e754219b5cd5fdcde9f
Wouterklein03 <w.klein@student.tudelft.nl>
2024-05-01 14:13:15 +0200

    .

--- GHSetStates.m
+++ GHSetStates.m
@@ -9,6 +9,6 @@ GH.x.            PlantTemperature(1) = 20 ;
 GH.x.            AirHumidity(1) = 0.012 ; % kg/m^3 air
 
 GH.x.            CO2Air(1) = 0.0464 ; % kg/m^3 air
-GH.x.            DryMassPlant = 10 * ones(1, length(t)) ; % Dry Mass plant (CO2)
+GH.x.            DryMassPlant = 100 * ones(1, length(t)) ; % Dry Mass plant (CO2)
 GH.x.            MassPlant = GH.x.DryMassPlant / 0.05 ; % Assume plant = ~95% water, (5% Dry Mass)
    
