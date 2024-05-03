08e565043881172bae308b04b930511f25a0f649
Wouterklein03 <w.klein@student.tudelft.nl>
2024-05-03 14:34:43 +0200

    .

--- Matrixmatrixmatrices/SetParameters.m
+++ Matrixmatrixmatrices/SetParameters.m
@@ -1,4 +1,63 @@
-% Set variables for matrices
+% Set variables (for matrices)
+
+% Greenhouse parameters                 ALL DUMMY!!!!!!!!!!!!!
+GH.p.           GHWidth = 10 ; %m 
+GH.p.           GHLength = 10 ; %m
+GH.p.           GHHeight = 3 ; %m
+GH.p.           GHWallThickness = 3e-3 ; %m
+GH.p.           GHFloorThickness = 1e-2 ;	%m
+
+GH.p.           NumberOfWindows = 10 ; 
+GH.p.           WindowLength = 0.2 ;
+GH.p.           WindowHeight = 0.1 ;
+GH.p.           RoofAngle = 10 ; % degrees
+
+GH.p.           WindowArea = GH.p.WindowHeight*GH.p.WindowLength ;
+GH.p.           GHVolume = GH.p.GHLength*GH.p.GHWidth*GH.p.GHHeight ;
+GH.p.           GHFloorArea = GH.p.GHLength * GH.p.GHWidth ;
+GH.p.           GHSideArea1 = GH.p.GHLength * GH.p.GHHeight ;
+GH.p.           GHSideArea2 = GH.p.GHWidth * GH.p.GHHeight ;
+GH.p.           GHTotalArea = GH.p.GHFloorArea + 2* GH.p.GHSideArea1 + 2* GH.p.GHSideArea2 ;
+GH.p.           GHPlantArea = 0.3 * GH.p.GHFloorArea ; %DUMMY
+
+% Plant parameters
+GH.p.           cp_lettuce = 4020 ;
+GH.p.           EmittancePlant = 0.90 ; %DUMMY
+GH.p.           YieldFactor = 0.544 ; %- (effective CO2 use efficiency)
+GH.p.           C_resp = 2.65e-7 ; %s^-1 (respiration rate in terms of respired dry matter)
+
+% Humidity equations parameters
+GH.p.           C_pld = 53 ; %m^2 kg^-1 (effective canopy surface)
+GH.p.           C_vplai = 3.6e-3 ; %m s^-1 (canopy transpiration mass transfer coefficient)
+GH.p.           C_v1 = 9348 ; %J m^-3 (parameter defining saturation water vapor pressure)
+GH.p.           C_v2 = 17.4 ; %K (parameter defining saturation water vapor pressure)    
+GH.p.           C_v3 = 239 ; %K (parameter defining saturation water vapor pressure)  
+
+% CO2 equations parameters
+GH.p.           C_RadPhoto = 3.55e10-9 ; %kg J^-1 (light use efficiency)
+GH.p.           C_R = 5.2e-5 ; %kg m^-3 (CO2 compensation point)
+GH.p.           C_CO21 = 5.11e-6 ; %m s^-1 K^-1 (temperature effect on CO2 diffiusion in leaves)
+GH.p.           C_CO22 = 2.3e-4 ; %m s^-1 K^-1 (temperature effect on CO2 diffiusion in leaves)
+GH.p.           C_CO23 = 6.29e-4 ; %m s^-1 K^-1 (temperature effect on CO2 diffiusion in leaves)
+
+% Ventilation parameters
+GH.p.           C_f = 0.6 ; % Discharge of energy by friction
+GH.p.           BetaAir = 1/283 ; % Thermal expansion coefficient
+
+% Temperature equations parameters       (Berg check ff hoe je dit allemaal wilt integreren in de rest van de code mbt overzichtelijkheid)
+
+AlfaGlass = 0.04 ; %DUMMY
+EmittanceGlass = 0.8 ; %DUMMY
+TauGlass = 0.80 ;
+EmittanceFloor = 0.9 ; %DUMMY
+KFloor = 0.3 ; %accurate enough for prototype
+LFloorGround = 19e-2 ; % meter
+EmittanceGlassSky = GH.p.EmittanceGlass ; %DUMMY
+
+
+
+
+
 
 A_plant= 86; A_cover= 2*1100; A_floor= 300;
 
--- Matrixmatrixmatrices/SetStates.m
+++ Matrixmatrixmatrices/SetStates.m
@@ -5,4 +5,13 @@ CoverTemperature = 10 ;
 FloorTemperature = 2 ;
 PlantTemperature = 25 ;
 
-Temperatures(:,1) = [AirTemperature; PlantTemperature; CoverTemperature; FloorTemperature];
+Temperatures(:,1) = [AirTemperature; PlantTemperature; CoverTemperature; FloorTemperature] ;
+
+
+Humidity = 0.012 ; % kg/m^3 air
+
+CO2Air = 0.0464 ; % kg/m^3 air
+DryMassPlant = 10  ; % Dry Mass plant (CO2)
+MassPlant = DryMassPlant / 0.05 ; % Assume plant = ~95% water, (5% Dry Mass)
+
+AddStates(:,1) = [Humidity; CO2Air; DryMassPlant; MassPlant] ;   % additional states
