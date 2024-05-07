398c8df2993fb2dc4cdb31fc8f95b2d3998a3861
Jelle04 <j.j.marcar@student.tudelft.nl>
2024-05-07 16:21:33 +0200

    pipe

--- heating_pipe.m
+++ heating_pipe.m
@@ -2,5 +2,8 @@
 m = 10; %mass flow through the pipe in kg/s
 T_in = 60; % temperature of the flow at the inlet in celsius
 T_out = 40; % temperature of the flow at the outlet in celsius
+T_air = 25; % temperature of the inside air
+
 %% outputs
-% temper
+% temperture outside pipe
+% total energy
