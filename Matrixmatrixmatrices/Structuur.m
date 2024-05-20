01d0a2e186639cf72b78c7b46411aa0334f9c4d6
Wouterklein03 <w.klein@student.tudelft.nl>
2024-05-20 10:17:03 +0200

    convection -> parameters

--- Matrixmatrixmatrices/Structuur.m
+++ Matrixmatrixmatrices/Structuur.m
@@ -136,6 +136,7 @@ for i = 1:length(t) - 1
     ConvectionCoefficientsIn(2,i) = h_ac ;
     ConvectionCoefficientsIn(3,i) = h_ac ;
     ConvectionCoefficientsIn(5,i) = h_ap ;
+    
     % Vapor flows and balance
     [W_trans(i), W_cond(i), W_vent(i)] = vaporflows(GH, T(1, i), T(3, i), OutsideTemperature(1,i), AddStates(1, i), OutsideHumidity(i), DryMassPlant, VentilationRate(i));
     HumidityDot = HumidityBalance(GH, W_trans(i), W_cond(i), W_vent(i));
