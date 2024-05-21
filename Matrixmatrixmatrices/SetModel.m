4996e813a3d453bccfb3468a24218c914dc74486
ldenijs1234 <162971452+ldenijs1234@users.noreply.github.com>
2024-05-21 11:49:47 +0200

    Update SetModel.m

--- Matrixmatrixmatrices/SetModel.m
+++ Matrixmatrixmatrices/SetModel.m
@@ -1,6 +1,6 @@
 % Create simulating parameters
 
-dt = 10 ;                                  % Time interval of 5 seconds
+dt = 100 ;                                  % Time interval of 5 seconds
 simulation_time =  24   *60*60 ;            % Simulation time in hours 
 start_time = 0 ;                           % Start of simulation
 
