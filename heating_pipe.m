6dc3cfcc7936e1c4972d86b2f150cabf7275c0aa
Jelle04 <j.j.marcar@student.tudelft.nl>
2024-05-21 09:27:26 +0200

    Update heating_pipe.m

--- heating_pipe.m
+++ heating_pipe.m
@@ -1,21 +1,21 @@
 %%inputs
-m = 10; %mass flow through the pipe in kg/s
+m = 0.1; %mass flow through the pipe in kg/s
 T_in = 60; % temperature of the flow at the inlet in celsius
-T_out = 40; % temperature of the flow at the outlet in celsius
 T_air = 25; % temperature of the inside air
 T_pipe = 50; % temperature of the outside of the pipe
 H_out = 0.012; % Kg/m^3
 C_out = 0.0464; % Kg/m^3
-L = 20; % length of the pipe
+L = 200; % length of the pipe
 F = 40; % Fins per meter of pipe %!!!!keep the thickness in mind not more fins then fit on the pipe!!!!
 g = 9.81;
 dT = T_pipe - T_air;
-r_0 = 0.025; % inside radius of the pipe in meters
-r_1 = 0.03; % outside radius of the pipe in meters
-r_2 = 0.05; % outside radius of the fin in meters
+r_0 = 0.05; % inside radius of the pipe in meters
+r_1 = 0.055; % outside radius of the pipe in meters
+r_2 = 0.1; % outside radius of the fin in meters
 k = 180; % conductive heat transfer coefficient of the material in W/(m*K)
 t = 0.003; % half of the thickness of one fin in meters
 A_in = pi*r_0^2; % Area of inside of the pipe
+k_alu = 237; %conduction coefficient aluminium for the pipe in W/(m*K)
 
 p = 1084; % air pressure in hpa
 h_abs = H_out*1000; % absolute humidity of the outside air in g/m^3
@@ -68,6 +68,23 @@ A = L*F*S+(L-L*F*2*t)*2*pi*r_1; % total area of the pipe in m^2
 Q_ideal = h_pipe*A*(T_pipe-T_air);
 Q = Q_ideal*Fin_efficiency
 
-mu = water.rho(T_in)*water.nu(T_in) %dynamic viscosity
-Re = (m/A_in)*2*r_0/mu
-%N_tu = h_c*A/(m*c_pH);
+%% Interpolation in table
+T = [275,280,285,290,295,300,310,320,330,340,350,360,370,373.15,380,390,400];
+Pr_array = [12.9,10.7,9,7.8,6.7,5.9,4.6,3.8,3.2,2.7,2.4,2,1.81,1.76,1.65,1.51,1.40];
+T_array = T-273.15;
+k_array = [0.556,0.568,0.58,0.591,0.602,0.611,0.628,0.641,0.652,0.661,0.669,0.676,0.68,0.681,0.683,0.684,0.685];
+c_p_array = [4217,4203,4192,4186,4181,4178,4174,4174,4178,4184,4190,4200,4209,4212,4220,4234,4250];
+k_water = interp1(T_array,k_array,T_in); %Conductive heat coefficient of water in W/(m*K)
+Pr = interp1(T_array,Pr_array,T_in); %parental number from table from "Basic heat and mass transfer"
+c_p = interp1(T_array,c_p_array,T_in);
+
+mu = water.rho(T_in)*water.nu(T_in); %dynamic viscosity
+Re = (m/A_in)*2*r_0/mu; %reynolds number
+f = (0.790*log(Re)-1.64)^(-2); % friction factor
+
+Nu_inside = ((f/8)*(Re-1000)*Pr/(1+(12.7*sqrt(f/8)*(Pr^(2/3) -1)))); %Nusselt number for inside the pipe
+h_inside = k_water*Nu_inside/(2*r_0);
+h_total = 1/((1/(h_inside*2*pi*r_0)) + (log(r_1/r_0)/(2*pi*k_alu)) + (1/(h_pipe*Fin_efficiency*F*S+h_pipe*(1-F*2*t)*2*pi*r_1)));% heat coefficient for the entire pipe in W/(m*K)
+N_tu = h_total*L/(m*c_p);
+epsi = 1-exp(-N_tu);
+T_out = T_in - epsi*(T_in-T_air)
