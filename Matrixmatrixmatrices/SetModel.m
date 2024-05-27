e2669feda2fac1c765d1ec77a88971e60b0505ca
Gijsdo <g.s.doorenbos@student.tudelft.nl>
2024-05-27 09:50:17 +0200

    Update SetModel.m

--- Matrixmatrixmatrices/SetModel.m
+++ Matrixmatrixmatrices/SetModel.m
@@ -1,6 +1,10 @@
 % Create simulating parameters
 
+<<<<<<< Updated upstream
 dt = 1 ;                                  % Time interval in seconds
+=======
+dt = 1 ;                                  % Time interval of 5 seconds
+>>>>>>> Stashed changes
 simulation_time =  24   *60*60 ;           % Simulation time in hours 
 total_time = 24         *60*60 ;           % Total time available for simulation (dependent on forecasts)  
 start_time = 0 ;                           % Start of simulation
