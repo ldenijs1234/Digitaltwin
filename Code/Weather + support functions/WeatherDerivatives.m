f13e3e4a66e188089db1c7ec89fcb2400ce11c15
Wouterklein03 <w.klein@student.tudelft.nl>
2024-06-03 12:05:21 +0200

    .

--- Code/Weather + support functions/WeatherDerivatives.m
+++ Code/Weather + support functions/WeatherDerivatives.m
@@ -1,13 +1,13 @@
 
 function Derivative = WeatherDerivatives(WeatherInput, dt)
 
-    Derivative = zeros(1, length(WeatherInput)) ;
+    Derivative = zeros(1, length(WeatherInput)) ;    % Empty array
 
     for i = 1:length(WeatherInput)-1
-        Derivative(i) = (WeatherInput(i+1) - WeatherInput(i))/dt ;
+        Derivative(i) = (WeatherInput(i+1) - WeatherInput(i))/dt ;   % Take forward derivative of desired input
     end
 
-    Derivative(end) = Derivative(end-1) ;
+    Derivative(end) = Derivative(end-1) ;   % Set last derivative for array size continuity
 end
 
 
