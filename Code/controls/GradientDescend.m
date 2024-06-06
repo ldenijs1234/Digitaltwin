3936848c41103e4b0cf00965ecb437fae34c2976
Wouterklein03 <w.klein@student.tudelft.nl>
2024-06-06 14:57:44 +0200

    gradient descend

--- Code/controls/GradientDescend.m
+++ Code/controls/GradientDescend.m
@@ -1,9 +1,7 @@
 run("SetModel")
-SimCount = 0;
-time_steps = simulation_time/dt+1 ;
-SimCount = SimCount + 1 ;                   % keep count on number of simulations done
-SimStart = time_steps*(SimCount-1) + 1 ;    % Define interval start based on SimCount
-SimEnd = time_steps*SimCount ;
+
+SimStart =  1 ;    % Define interval start based on SimCount
+SimEnd = length(t) ;
 % Compute lower and upper bounds
 Lowerbound_total = bound(10, 15, 6, 10, 18, 22, dt, total_time / 24); % Setpoint temperature (°C)
 Upperbound_total = bound(20, 25, 6, 10, 18, 22, dt, total_time / 24); % Setpoint temperature (°C)
@@ -26,29 +24,25 @@ T_st = Lowerbound(1:3600/dt:end)' + 1;
 
 % Initialize waitbar
 hWaitBar2 = waitbar(0, 'Please wait...');
-
-cost = inf * ones(size(T_st(:, 1)));
+cost = inf * ones(size(T_st));
 TC_Count = 0;
-delta = 0.01;
-alfa = 0.5;
+alfa = 0.1;
 % Number of iterations
 iteration_amount = 10;
 n = 1;
 
 % Interpolate setpoints over time
-Setpoint = interp1(0:24, T_st(:, 1), t / 3600, 'linear', 'extrap');
+function cost = cost_set(T_st)
+    Setpoint = interp1(0:24, T_st(:, 1), t / 3600, 'linear', 'extrap');
 
-% Run the initial simulation with the initial setpoints
-run("Initialize")
-BoundBreak = true;
-run("RunFullSim")
-
-if Belowbound == false
-
-    costT_st =  sum(Energy_kWh .* simdaycost(1:end-1)) ;
-    cost_save(1) = costT_st;
-
+    % Run the initial simulation with the initial setpoints
+    run("Initialize")
+    BoundBreak = true;
+    run("RunFullSim")
+    cost =  sum(Energy_kWh .* simdaycost(1:end-1)) ;
 end
+
+
 
 while Belowbound == true
     disp('increase setpoint by 1')
@@ -66,57 +60,27 @@ while Belowbound == true
     end
 end
 
+cost_nul = cost(T_st) ;
 
 for n = 2:iteration_amount
     waitbar(n/iteration_amount, hWaitBar2, sprintf('Iteration %d/%d', n, iteration_amount))
+    delta = randi([0.01, 0.05], size(T_st));
+    T_st_delta = T_st + delta ;
 
-    for m = randperm(25)    
-        % set and run test 
-        T_st_test = T_st;
+    costT_st = cost(T_st) ;
+    costT_st_delta = cost(T_st_delta) ;
 
-        T_st_new = T_st;
+    gradient = (costT_st_delta - costT_st) ./ delta ;
 
-        Setpoint = interp1(0:24, T_st_test, t / 3600, 'linear', 'extrap');
-            
-        run("Initialize")
-        BoundBreak = true;
-        run("RunFullSim")
+    T_st_new = T_st - alpha * gradient ;
 
-        if Belowbound == false
-            %new T_st function if T_st_test wasnt below bound
-            costT_st_test =  sum(Energy_kWh .* simdaycost(1:end-1)) ;
-            T_st_new(m) = max(T_st(m) - alfa * (costT_st_test - costT_st) / delta, Lowerbound((m-1) * 3600/dt + 1) + 0.5) ;
-        % else
-        %     %re-run with opposite delta 
-        %     T_st_test = T_st;
-        %     T_st_test(m) = T_st(m) - delta;
+    cost_new = cost(T_st_new);
 
-        %     Setpoint = interp1(0:24, T_st_test, t / 3600, 'linear', 'extrap');
-            
-        %     run("Initialize")
-        %     BoundBreak = true;
-        %     run("RunFullSim")
-
-        %     if Belowbound == false
-        %         %new T_st function if T_st_test wasnt below bound
-        %         costT_st_test =  sum(Energy_kWh .* simdaycost(1:end-1)) ;
-        %         T_st_new(m) = max(T_st(m) - alfa * (costT_st_test - costT_st) / (-delta), Lowerbound(m * 3600/dt) + 0.5) ;
-            % end
-
-        end
-
-        Setpoint = interp1(0:24, T_st_new(:, 1), t / 3600, 'linear', 'extrap');
-        run("Initialize")
-        BoundBreak = true;
-        run("RunFullSim")
-
-        if Belowbound == false %&& costnew < cost?
-            costT_st =  sum(Energy_kWh .* simdaycost(1:end-1)) ;
-            T_st = T_st_new;
-        end
-
-
+    if Belowbound == false %&& costnew < cost?
+        T_st = T_st_new;
+        costT_st = cost_new;
     end
+
 
     T_st_save(:,n) = T_st;
     cost_save(n) = costT_st;
