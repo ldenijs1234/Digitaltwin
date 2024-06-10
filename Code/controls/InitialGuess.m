c3cebb0633123161ebaae6ec5ac588416c630579
Gijsdo <g.s.doorenbos@student.tudelft.nl>
2024-06-10 11:31:53 +0200

    je ma

--- Code/controls/InitialGuess.m
+++ Code/controls/InitialGuess.m
@@ -3,9 +3,15 @@ run("SetInputs")
 
 Cost_Derivative = diff(simdaycost)./diff(t);
 
+window_size = 60;
+
+function [output_array, window] = weighted_average_with_hann_window(input_array, window_size)
+      
     % Create a Hann window of the specified size
+    hann_window = hann(window_size);
     
     % Normalize the window to make sure it sums to 1
+    window = hann_window / sum(hann_window);
     
     % Initialize the result array
     result_length = length(input_array) - window_size + 1;
@@ -17,3 +23,4 @@ Cost_Derivative = diff(simdaycost)./diff(t);
     end
 end
 
+[weighted_average, hann_window] = weighted_average_with_hann_window(Cost_Derivative, window_size);
